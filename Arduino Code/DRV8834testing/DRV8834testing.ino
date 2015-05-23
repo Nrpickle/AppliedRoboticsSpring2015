1cb3c719c83140316c42c88c7b0519be76db144f
Nick McComb <nick@nickmccomb.net>
2015-05-22 21:59:45 -0700

    Full rotation, 360 per direction

--- Arduino Code/DRV8834testing/DRV8834testing.ino
+++ Arduino Code/DRV8834testing/DRV8834testing.ino
@@ -6,26 +6,37 @@ const int ledPin = 13;
 
 void setup(){
   pinMode(stepPin, OUTPUT);
-  pinMode(dirPin, OUTPUT):
+  pinMode(dirPin, OUTPUT);
   digitalWrite(stepPin, LOW);
   digitalWrite(dirPin, LOW); 
  
+  Serial.begin(9600);
+  
   pinMode(ledPin, OUTPUT);
   digitalWrite(ledPin, OUTPUT); 
 }
 
+short counter = 0;
 
 void loop(){
   digitalWrite(ledPin, HIGH);
   
-  for(int i = 0; i < 200){
+  for(int i = 0; i < 218; ++i){
     digitalWrite(stepPin, HIGH);
-    delayMicroseconds(100);
+    delayMicroseconds(500);
     digitalWrite(stepPin, LOW);
-    delayMicroseconds(100);
+    delayMicroseconds(1000);
+  }
+  
+  ++counter;
+  
+  if (counter == 4) {
+    Serial.println("# Changing Direction #");
+    counter = 0;
+    digitalWrite(dirPin, !digitalRead(dirPin));
   }
   
   digitalWrite(ledPin, LOW);
   
-  delay(1000);
+  delay(500);
 }
